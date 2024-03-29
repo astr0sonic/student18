74ec300a43860861cfca835c6fe86002576f67e8
onyx <anvarnurgaliev0@gmail.com>
2024-03-29 12:29:02 +0300

    commit1

---
+++ sundaram.cpp
@@ -0,0 +1,19 @@
+#include "sundaram.h"
+#include <vector>
+using namespace std;
+
+std::vector<int> sieve(int k) {
+    vector<bool> f(k + 1);
+    vector<int> primes = { 2 };
+    for (int i = 0; i <= k; i++)
+        f[i] = true;
+
+    for (int i = 1; i <= (sqrt(2 * k + 1) - 1) / 2; i++)
+        for (int j = i; i + j + 2 * i * j <= k; j++)
+            f[i + j + 2 * i * j] = false;
+
+    for (int i = 1; i <= k; i++)
+        if (f[i])
+            primes.push_back(i * 2 + 1);
+    return primes;
+}
