82496d1d7951acf88edd71bb5d5528729d11723e
onyx <anvarnurgaliev0@gmail.com>
2024-04-04 19:06:30 +0300

    commit2

--- volume.cpp
+++ volume.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int getMaxVolume(int a, int b, int c, int n, int* arr)
 {
     int mx = 0;
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
-        {
-            int mn = min({ arr[i], arr[j], c });
-            int t = (j - i < a) ? (j - i) * mn : a * mn;
-            if (mx < t)
-                mx = t;
-        }
+    int i = 0, j = n - 1;
+    while (i != j)
+    {
+        int mn = min({ arr[i], arr[j], c });
+        int t = (j - i < a) ? (j - i) * mn : a * mn;
+        if (mx < t)
+            mx = t;
+        if (arr[i] <= arr[j])
+            i++;
+        else
+            j--;
+    }
     return b * mx;
 }
