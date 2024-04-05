a3314dcced92793e819baa9a21007827b7dd5503
onyx <anvarnurgaliev0@gmail.com>
2024-04-05 12:34:30 +0300

    commit1

---
+++ bracket.cpp
@@ -0,0 +1,48 @@
+#include "bracket.h"
+#include <stack>
+
+using namespace std;
+
+bool checkBrackets(const std::string& s) {
+    stringstream str(s);
+    return task5(s);
+}
+
+bool task5(istream& f)
+{
+    stack<char> q; char x;
+    while (!f.eof())
+    {
+        x = f.get();
+        switch (x)
+        {
+        case '(':
+        case '{':
+        case '<':
+        case '[':
+            q.push(x);
+            break;
+        case ')':
+            if (q.empty() || q.top() != '(')
+                return false;
+            q.pop();
+            break;
+        case '}':
+            if (q.empty() || q.top() != '{')
+                return false;
+            q.pop();
+            break;
+        case ']':
+            if (q.empty() || q.top() != '[')
+                return false;
+            q.pop();
+            break;
+        case '>':
+            if (q.empty() || q.top() != '<')
+                return false;
+            q.pop();
+            break;
+        }
+    }
+    return q.empty();
+}
---
+++ bracket.h
@@ -0,0 +1,6 @@
+#include <string>
+#include <sstream>
+
+bool checkBrackets(const std::string& s);
+
+bool task5(std::istream& f);
