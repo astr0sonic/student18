df381d1c1ee8a8556c5427fd702212dcbb5460d6
onyx <anvarnurgaliev0@gmail.com>
2024-04-09 10:38:22 +0300

    commit2

--- rpn.cpp
+++ rpn.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-bool isznak(char a) { return a == '+' || a == '-' || a == '*'; }
+bool isznak(char a) { return a == '+' || a == '-' || a == '*' || a == '/' || a == '^'; }
 
 double calculateRPN(const string& s) {
 	stack<double> a;
