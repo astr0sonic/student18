bc34e7a74e0b0987fdd4a6bd54da5b7d7659e39e
onyx <anvarnurgaliev0@gmail.com>
2024-04-09 10:34:06 +0300

    commit1

---
+++ rpn.cpp
@@ -0,0 +1,46 @@
+#include "rpn.h"
+#include <stack>
+#include <cmath>
+using namespace std;
+
+bool isznak(char a) { return a == '+' || a == '-' || a == '*'; }
+
+double calculateRPN(const string& s) {
+	stack<double> a;
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != ' ')
+		{
+			if (!isznak(s[i]))
+				a.push(s[i] - '0');
+			else
+			{
+				double z;
+				double x = a.top(); a.pop();
+				double y = a.top(); a.pop();
+				switch (s[i])
+				{
+				case '+':
+					z = x + y;
+					break;
+				case '-':
+					z = y - x;
+					break;
+				case '*':
+					z = x * y;
+					break;
+				case '/':
+					z = y / x;
+					break;
+				case '^':
+					z = pow(y, x);
+					break;
+				}
+				a.push(z);
+			}
+		}
+		i++;
+	}
+	return a.top();
+}
